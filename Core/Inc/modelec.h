bab28e88a0ccd59bc0507cc3c2b1396ca23e94d9
acki <ackimixs@protonmail.com>
2026-03-24 17:04:22 +0100

    variable

--- Core/Inc/modelec.h
+++ Core/Inc/modelec.h
@@ -64,7 +64,7 @@ public:
 
 	float preciseAngleFinal = 0.002f;
 	float preciseAngle = 0.39f;
-	float precisePosFinal = 0.003f;
+	float precisePosFinal = 0.001f;
 	float precisePos = 0.15f;
 
 	float currentV = 0;
